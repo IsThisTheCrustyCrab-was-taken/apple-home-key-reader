bd24f27d4c821847d777849743464680d63cb24f
IsThisTheCrustyCrab-was-taken <kampe@uni-potsdam.de>
2023-12-31 00:19:40 +0100

    running a doorlock on raspi is shockingly easy

---
+++ lock.py
@@ -0,0 +1,75 @@
+"""
+Pins for the lock are hardcoded for now
+
+    Function       |Pin (BCM GPIO - whatever that means)
+    ---------------|-----------------
+Inputs:            |
+    Door switch    |  6
+    Top endstop    | 13
+    Bottom endstop | 22
+    (Contact)      | 26
+    // Set Contact to 1 and pull the inputs to ground
+Motor:             |
+    Pwm            |  2
+    Up             |  3
+    Down           |  4
+    // Set either up and down high to move in that direction, enable both to brake
+"""
+
+from enum import Enum
+from gpiozero import Motor, DigitalInputDevice, DigitalOutputDevice, Button
+
+
+class DoorLock:
+    def __init__(self, on_open=None, on_close=None):
+        # Input pins
+        top_endstop_pin = 13
+        bottom_endstop_pin = 22
+        door_switch_pin = 6
+        contact_pin = 26
+        # Motor pins
+        pwm_pin = 2
+        up_pin = 3
+        down_pin = 4
+
+        self.on_open = on_open
+        self.on_close = on_close
+
+        self.motor_speed = 0.25
+        # Setup Input pins
+        self.motor = Motor(forward=up_pin, backward=down_pin, enable=pwm_pin)
+
+        self.top_endstop = DigitalInputDevice(top_endstop_pin)
+        self.bottom_endstop = DigitalInputDevice(bottom_endstop_pin)
+        self.door_switch = Button(door_switch_pin)
+        self.contact = DigitalOutputDevice(contact_pin)
+
+        self.door_switch.hold_time = 2
+        self.contact.off()
+        self.top_endstop.when_activated = self.on_opened
+        self.bottom_endstop.when_activated = self.on_closed
+        self.door_switch.when_held = self.close
+
+    def close(self):
+        self.motor.forward(self.motor_speed)
+
+    def open(self):
+        self.motor.backward(self.motor_speed)
+
+    def on_opened(self):
+        self.motor.stop()
+        self.on_open()
+
+    def on_closed(self):
+        self.motor.stop()
+        self.on_close()
+
+    @property
+    def opened(self):
+        return self.top_endstop.is_active
+
+    @property
+    def closed(self):
+        return self.bottom_endstop.is_active
+
+
