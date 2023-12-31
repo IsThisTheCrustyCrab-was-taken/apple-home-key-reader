eac7a925741e42cb2b55aa71ae033063d338a5fe
IsThisTheCrustyCrab-was-taken <kampe@uni-potsdam.de>
2023-12-31 03:00:35 +0100

    rollback, something wrong

--- accessory.py
+++ accessory.py
@@ -35,12 +35,12 @@ class Lock(Accessory):
         # TODO: motor movement should be done here (probably in the function below)
         self.lock_target_state.set_value(self._lock_target_state, should_notify=True)
         if self._lock_target_state == 1:
-            self.lock.close()
+            self.lock.open()
         else:
             self.lock.close()
 
     def lock_done(self):
-        self._lock_current_state = 0 if self.lock.closed else 1
+        self._lock_current_state = 1 if self.lock.closed else 0
         self.lock_current_state.set_value(self._lock_current_state, should_notify=True)
 
     def add_preload_service(self, service, chars=None, unique_id=None):
@@ -75,14 +75,14 @@ class Lock(Accessory):
 
         self.lock_current_state = self.service_lock_mechanism.configure_char(
             # TODO: Needs to be changed according to the current state on startup
-            "LockCurrentState", getter_callback=self.get_lock_current_state, value=0 if self.lock.closed else 1
+            "LockCurrentState", getter_callback=self.get_lock_current_state, value=1 if self.lock.closed else 0
         )
 
         self.lock_target_state = self.service_lock_mechanism.configure_char(
             "LockTargetState",
             getter_callback=self.get_lock_target_state,
             setter_callback=self.set_lock_target_state,
-            value=0 if self.lock.closed else 1,
+            value=1 if self.lock.closed else 0,
         )
 
         self.service_lock_management = self.add_preload_service("LockManagement")
@@ -124,7 +124,7 @@ class Lock(Accessory):
 
     def get_lock_current_state(self):
         log.info("get_lock_current_state")
-        self._lock_current_state = 0 if self.lock.closed else 1
+        self._lock_current_state = 1 if self.lock.closed else 0
         return self._lock_current_state
 
     def get_lock_target_state(self):
@@ -135,7 +135,7 @@ class Lock(Accessory):
         log.info(f"set_lock_target_state {value}")
         self._lock_target_state = value
         if self._lock_target_state == 1:
-            self.lock.close()
+            self.lock.open()
         else:
             self.lock.close()
         return self._lock_target_state
